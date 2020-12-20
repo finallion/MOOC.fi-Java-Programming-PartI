4be035ec8686e4ab1e6f708105422b2c7099a463
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 19:07:57 +0100

    Update AverageOfTwoNumbers.java

--- Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -6,7 +6,15 @@ public class AverageOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number: ");
+        int firstInput = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number: ");
+        int secondInput = Integer.valueOf(scanner.nextLine());
+
+        double sum = (double) firstInput + secondInput;
+
+        System.out.println("The average is " + sum/2);
 
     }
 }
