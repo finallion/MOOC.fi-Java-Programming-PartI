6447a87436d5d396f2aefd1c2825bdac639531ce
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 19:11:22 +0100

    Update SimpleCalculator.java

--- Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -5,8 +5,22 @@ public class SimpleCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give the first number: ");
+        int firstInput = Integer.valueOf(scanner.nextLine());
 
-        // Write your program here
+        System.out.println("Give the second number: ");
+        int secondInput = Integer.valueOf(scanner.nextLine());
+
+        int addition = firstInput + secondInput;
+        int substraction = firstInput - secondInput;
+        int multiplication = firstInput * secondInput;
+        double division = (double) firstInput / secondInput;
+        
+        System.out.println(firstInput + " + " + secondInput + " = " + addition);
+        System.out.println(firstInput + " - " + secondInput + " = " + substraction);
+        System.out.println(firstInput + " * " + secondInput + " = " + multiplication);
+        System.out.println(firstInput + " / " + secondInput + " = " + division);
 
     }
 }
