2e422614b01709267c74f76fd95c8552485193d0
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 18:56:21 +0100

    Update SecondsInADay.java

--- Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -5,8 +5,14 @@ public class SecondsInADay {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("How many days would you like to convert to seconds?");
+        int days = Integer.valueOf(scanner.nextLine());
 
-        // Write your program here
+ 
+        int secondsPerDay = 60 * 60 * 24;
+        int secondsPerNDays = secondsPerDay * days;
 
+        System.out.println(secondsPerNDays);
     }
 }
