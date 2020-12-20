4d063d672580593c8bbc1c676842bf71d339755a
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 19:25:39 +0100

    Update CheckingTheAge.java

--- Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
+++ Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
@@ -5,6 +5,14 @@ public class CheckingTheAge {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        System.out.println("How old are you?");
+        int input = Integer.valueOf(scan.nextLine());
 
+        if (input >= 0 && input <= 120) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Impossible!");
+        }
     }
 }
