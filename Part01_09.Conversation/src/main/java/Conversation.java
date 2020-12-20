d00dbf9704da3c84296ec86e13e570dd36c4287e
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 18:48:53 +0100

    Update Conversation.java

--- Part01_09.Conversation/src/main/java/Conversation.java
+++ Part01_09.Conversation/src/main/java/Conversation.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 public class Conversation {
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Greetings! How are you doing?");
+        String firstUserInput = scanner.nextLine();
 
-        // Write your program here
+        System.out.println("Oh, how interesting. Tell me more!");
+        String secondUserInput = scanner.nextLine();
+
+        System.out.println("Thanks for sharing!");
 
     }
 }
