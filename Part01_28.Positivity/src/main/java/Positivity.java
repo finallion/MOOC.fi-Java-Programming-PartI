12ee8a327d1ec3d65884f436970d27619fa9a2e7
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 19:18:17 +0100

    Update Positivity.java

--- Part01_28.Positivity/src/main/java/Positivity.java
+++ Part01_28.Positivity/src/main/java/Positivity.java
@@ -5,7 +5,15 @@ public class Positivity {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Give a number: ");
 
-        // Write your program here
+        int input = Integer.valueOf(scan.nextLine());
+        
+        if (input > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
     }
 }
