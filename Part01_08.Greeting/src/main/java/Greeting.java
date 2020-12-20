47546c959467ecf5dbe3ff71845aef874b7d3da3
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 18:47:55 +0100

    Update Greeting.java

--- Part01_08.Greeting/src/main/java/Greeting.java
+++ Part01_08.Greeting/src/main/java/Greeting.java
@@ -5,8 +5,11 @@ public class Greeting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("What's your name?");
 
-        // Write your program here
+        String userInput = scanner.nextLine();
+
+        System.out.println("Hi " + userInput);
 
     }
 }
