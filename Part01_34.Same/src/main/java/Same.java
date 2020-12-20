3f6a0c27dd75cf6bb40040c677a21f9db565834e
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 19:24:45 +0100

    Update Same.java

--- Part01_34.Same/src/main/java/Same.java
+++ Part01_34.Same/src/main/java/Same.java
@@ -5,7 +5,17 @@ public class Same {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Enter the first string: ");
+        String inputOne = scan.nextLine();
 
-        // Write your program here. 
+        System.out.println("Enter the second string: ");
+        String inputTwo = scan.nextLine();
+
+        if (inputOne.equals(inputTwo)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        } 
     }
 }
