132dcf27fd1946376ed6db2e229aa1cb3d72ef65
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 18:49:56 +0100

    Update Story.java

--- Part01_10.Story/src/main/java/Story.java
+++ Part01_10.Story/src/main/java/Story.java
@@ -5,8 +5,19 @@ public class Story {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("I will tell you a story, but I need some information first.");
+        
+        System.out.println("What is the main character called?");
+        String firstUserInput = reader.nextLine();
 
-        // Write your program here
+        System.out.println("What is their job?");
+        String secondUserInput = reader.nextLine();
+
+        System.out.println("Here is the story:");
+        System.out.println("Once upon a time there was " + firstUserInput + ", who was " + secondUserInput + ".");
+        System.out.println("On the way to work, " + firstUserInput + " reflected on life.");
+        System.out.println("Perhaps " + firstUserInput + " will not be " + secondUserInput + " forever.");
 
     }
 }
