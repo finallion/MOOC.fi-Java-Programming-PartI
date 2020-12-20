8c51accaa199cc8afc1c1636649309eb60766753
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 19:09:44 +0100

    Update AverageOfThreeNumbers.java

--- Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
+++ Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
@@ -6,7 +6,18 @@ public class AverageOfThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number: ");
+        int firstInput = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number: ");
+        int secondInput = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the third number: ");
+        int thirdInput = Integer.valueOf(scanner.nextLine());
+
+        double sum = (double) firstInput + secondInput + thirdInput;
+
+        System.out.println("The average is " + sum/3);
 
     }
 }
