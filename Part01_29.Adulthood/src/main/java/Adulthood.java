4376af19ccd82994b3243f81def7fdad345f1a7d
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 19:19:13 +0100

    Update Adulthood.java

--- Part01_29.Adulthood/src/main/java/Adulthood.java
+++ Part01_29.Adulthood/src/main/java/Adulthood.java
@@ -5,7 +5,14 @@ public class Adulthood {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("How old are you?");
 
-        // Write your program here 
+        int x = Integer.valueOf(scan.nextLine());
+
+        if (x >= 18) {
+            System.out.println("You are an adult");
+        } else {
+            System.out.println("You are not an adult");
+        }
     }
 }
