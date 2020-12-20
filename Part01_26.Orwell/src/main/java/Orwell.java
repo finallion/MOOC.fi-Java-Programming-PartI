5cbac3d4eb14fc1ea4249a83cb45d7dd670e257a
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 19:15:21 +0100

    Update Orwell.java

--- Part01_26.Orwell/src/main/java/Orwell.java
+++ Part01_26.Orwell/src/main/java/Orwell.java
@@ -5,7 +5,13 @@ public class Orwell {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Give a number: ");
+        int input = Integer.valueOf(scan.nextLine());
+        int orwellYear = 1984;
 
-        // Write your program here
+        if (input == orwellYear) {
+            System.out.println("Orwell");
+        }
     }
 }
