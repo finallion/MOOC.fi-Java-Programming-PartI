b1317ecc2952fadcc7e41780d009e0f484cbc4e3
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 19:12:45 +0100

    Update SpeedingTicket.java

--- Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
+++ Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
@@ -5,7 +5,12 @@ public class SpeedingTicket {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give speed: ");
+        int speed = Integer.valueOf(scanner.nextLine());
 
-        // Write your program here. 
+        if (speed > 120) {
+            System.out.println("Speeding ticket!");
+        }
     }
 }
