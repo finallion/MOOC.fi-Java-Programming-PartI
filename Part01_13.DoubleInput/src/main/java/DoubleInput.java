370cdd3789e806a1b172d4f3c5655cceb6582eb8
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 18:52:39 +0100

    Update DoubleInput.java

--- Part01_13.DoubleInput/src/main/java/DoubleInput.java
+++ Part01_13.DoubleInput/src/main/java/DoubleInput.java
@@ -5,8 +5,12 @@ public class DoubleInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give a number:");
 
-        // write your program here
+        double userInput = Double.valueOf(scanner.nextLine());
+
+        System.out.println("You gave the number " + userInput);
 
     }
 }
