04d8321a11f9d2977b91e554b537c579f7f33c87
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 19:21:12 +0100

    Update GradesAndPoints.java

--- Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
+++ Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
@@ -5,6 +5,27 @@ public class GradesAndPoints {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Give points [0-100]: ");
+        int x = Integer.valueOf(scan.nextLine());
+        
+        if (x > 100) {
+            System.out.println("incredible!");
+        } else if (x >= 90) {
+            System.out.println("Grade: 5");
+        } else if (x >= 80) {
+            System.out.println("Grade: 4");
+        } else if (x >= 70) {
+            System.out.println("Grade: 3");
+        } else if (x >= 60) {
+            System.out.println("Grade: 2");
+        } else if (x >= 50) {
+            System.out.println("Grade: 1");
+        } else if (x >= 0){
+            System.out.println("failed");
+        } else {
+            System.out.println("impossible!");
+        }
 
     }
 }
