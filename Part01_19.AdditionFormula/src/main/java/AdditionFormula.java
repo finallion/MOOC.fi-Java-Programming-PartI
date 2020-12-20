00889be00f4400e8c7fbdf81b3adb6133178a284
finallion <74151350+finallion@users.noreply.github.com>
2020-12-20 18:59:07 +0100

    Update AdditionFormula.java

--- Part01_19.AdditionFormula/src/main/java/AdditionFormula.java
+++ Part01_19.AdditionFormula/src/main/java/AdditionFormula.java
@@ -6,7 +6,14 @@ public class AdditionFormula {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // write your program here
+        System.out.println("Give the first number: ");
+        int firstInput = Integer.valueOf(scanner.nextLine());
 
+        System.out.println("Give the second number: ");
+        int secondInput = Integer.valueOf(scanner.nextLine());
+        
+        int sum = firstInput + secondInput;
+
+        System.out.println(firstInput + " + " + secondInput + " = " + sum);
     }
 }
