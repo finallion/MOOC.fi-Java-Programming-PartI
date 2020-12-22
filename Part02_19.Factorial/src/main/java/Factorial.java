db6588d9af7d16cc45b64a662029e767a97fede6
finallion <74151350+finallion@users.noreply.github.com>
2020-12-22 13:35:40 +0100

    Update Factorial.java

--- Part02_19.Factorial/src/main/java/Factorial.java
+++ Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,15 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number: ");
 
+        int first = Integer.valueOf(scanner.nextLine());
+        int sum = 1;
+
+        for (int x = 1; x <= first; x++) {
+            sum = sum * x;
+        }
+        
+        System.out.println("The sum is " + sum);
     }
 }
