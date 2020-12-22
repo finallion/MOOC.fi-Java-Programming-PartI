7a3accaeba820635367db8e847e1d8760f69992d
finallion <74151350+finallion@users.noreply.github.com>
2020-12-22 14:09:12 +0100

    Update SumOfNumbers.java

--- Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -5,6 +5,18 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+
+        while (true) {
+            System.out.println("Give a number: ");
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0) {
+                break;
+            }
+            counter = counter + input;
+
+        }
+        System.out.println("Sum of the numbers: " + counter);
 
     }
 }
