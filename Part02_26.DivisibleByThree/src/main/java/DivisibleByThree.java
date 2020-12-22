43b9fe6af1c53f820a87803cad7022c6ded2059a
finallion <74151350+finallion@users.noreply.github.com>
2020-12-22 13:46:49 +0100

    Update DivisibleByThree.java

--- Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -5,6 +5,17 @@ public class DivisibleByThree {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        divisibleByThreeInRange(9, 12);
+    }
+    
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        while (beginning <= end) {
+            int remainder = beginning%3;
+            if (remainder == 0) {
+                System.out.println(beginning);
+            }
+            beginning++;
+        }
 
     }
 
