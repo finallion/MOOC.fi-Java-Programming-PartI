cdeb2656925a23ebb94be7cb81bd871662119946
finallion <74151350+finallion@users.noreply.github.com>
2020-12-22 13:51:39 +0100

    Update Averaging.java

--- Part02_32.Averaging/src/main/java/Averaging.java
+++ Part02_32.Averaging/src/main/java/Averaging.java
@@ -1,15 +1,15 @@
 
 public class Averaging {
 
-    // implement the sum method here again
     public static int sum(int number1, int number2, int number3, int number4) {
-        // write some code here
-        return 0;
+        int x = number1 + number2 + number3 + number4;
+        return x;
     }
 
     public static double average(int number1, int number2, int number3, int number4) {
-        //write some code here
-        return -1;
+        int all = sum(number1, number2, number3, number4);
+        double avg = (double) all / 4;
+        return avg;
     }
 
     public static void main(String[] args) {
