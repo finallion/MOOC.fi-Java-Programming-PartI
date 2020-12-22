c9c9497f55f6240ede8227f8ccf74d35a305f8f0
finallion <74151350+finallion@users.noreply.github.com>
2020-12-22 13:26:08 +0100

    Update Squared.java

--- Part02_01.Squared/src/main/java/Squared.java
+++ Part02_01.Squared/src/main/java/Squared.java
@@ -6,5 +6,8 @@ public class Squared {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int input = Integer.valueOf(scanner.nextLine());
+        System.out.println(input*input);
+
     }
 }
