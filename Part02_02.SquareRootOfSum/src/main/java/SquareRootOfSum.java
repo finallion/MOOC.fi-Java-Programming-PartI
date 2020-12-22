b3f1520f58769763b4ad52a16b2a8979f7f78220
finallion <74151350+finallion@users.noreply.github.com>
2020-12-22 13:59:52 +0100

    Update SquareRootOfSum.java

--- Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,11 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int inputOne = Integer.valueOf(scanner.nextLine());
+        int inputTwo = Integer.valueOf(scanner.nextLine());
+        int sum = inputOne + inputTwo;
+        double squareRoot = Math.sqrt(sum);
+        System.out.println(squareRoot);
 
     }
 }
