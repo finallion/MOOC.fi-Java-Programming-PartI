bb645c7fb26d3f8ae7f8d04e80d0dc61dbadfce6
finallion <74151350+finallion@users.noreply.github.com>
2020-12-22 13:34:44 +0100

    Update SumOfASequence.java

--- Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -5,6 +5,19 @@ public class SumOfASequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Last number? ");
+
+        int inputLastNumber = Integer.valueOf(scanner.nextLine());
+
+        int sum = 0;
+        int number = 1;
+
+        while (number <= inputLastNumber) {
+            sum += number;
+            number++;
+        }
+
+        System.out.println("The sum is " + sum);
 
     }
 }
