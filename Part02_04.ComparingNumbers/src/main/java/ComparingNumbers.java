c0f4b5b352b7d0c8f972eddfadd12bda54d78303
finallion <74151350+finallion@users.noreply.github.com>
2020-12-22 14:01:31 +0100

    Update ComparingNumbers.java

--- Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
+++ Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -5,6 +5,15 @@ public class ComparingNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int x = Integer.valueOf(scanner.nextLine());
+        int y = Integer.valueOf(scanner.nextLine());
 
+        if (x > y) {
+            System.out.println(x + " is greater than " + y);
+        } else if (x < y) {
+            System.out.println(x + " is smaller than " + y);
+        } else {
+            System.out.println(x + " is equal to " + y);
+        }
     }
 }
