04386dabe945132be2c3d01f767a5b29a8d2bed4
finallion <74151350+finallion@users.noreply.github.com>
2020-12-22 14:23:21 +0100

    Update SumOfASequenceTheSequel.java

--- Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -5,6 +5,25 @@ public class SumOfASequenceTheSequel {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("First number?");
+        int first = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Last number?");
+        int last = Integer.valueOf(scanner.nextLine());
+        
+        int sum = 0;
+
+        while (first <= last) {
+            sum = sum + first;
+            first++;
+        }
+
+        //with for-loop
+        /*for (int x = first; x <= last; x++) {
+            sum += x;
+        } */
+
+        System.out.println("The sum is " + sum);
 
     }
 }
