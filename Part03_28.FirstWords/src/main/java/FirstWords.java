94ee1214fa02b68f243f44dd8beb9c019d8bc4b5
finallion <74151350+finallion@users.noreply.github.com>
2020-12-27 16:54:43 +0100

    Update FirstWords.java

--- Part03_28.FirstWords/src/main/java/FirstWords.java
+++ Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -5,7 +5,16 @@ public class FirstWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String input = scanner.nextLine();
+            String[] fragment = input.split(" ");
 
+            if (input.equals("")) {
+                break;
+            }
 
+            System.out.println(fragment[0]);
+        }
     }
 }
