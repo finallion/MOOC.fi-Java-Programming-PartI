2fffee2782ece878ab761c31fe2e98d166d82294
finallion <74151350+finallion@users.noreply.github.com>
2020-12-27 16:55:36 +0100

    Update LastWords.java

--- Part03_29.LastWords/src/main/java/LastWords.java
+++ Part03_29.LastWords/src/main/java/LastWords.java
@@ -5,7 +5,18 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String input = scanner.nextLine();
+            String[] fragment = input.split(" ");
 
+            if (input.equals("")) {
+                break;
+            }
 
+            int last = fragment.length - 1;
+            System.out.println(fragment[last]);   
+
+        }
     }
 }
