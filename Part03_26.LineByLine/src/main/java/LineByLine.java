4978cb16dfd701327bf1432dec0112fa271d89dc
finallion <74151350+finallion@users.noreply.github.com>
2020-12-27 16:52:32 +0100

    Update LineByLine.java

--- Part03_26.LineByLine/src/main/java/LineByLine.java
+++ Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,18 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String input = scanner.nextLine();
+            String[] pieces = input.split(" ");
 
+            for (int x = 0; x < pieces.length; x++) {
+                System.out.println(pieces[x]);
+            }
 
+            if (input.equals("")) {
+                break;
+            }
+        }
     }
 }
