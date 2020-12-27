fe4926b432ff2dfb285bed61936efd169eb4c419
finallion <74151350+finallion@users.noreply.github.com>
2020-12-27 16:39:38 +0100

    Update AverageOfAList.java

--- Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
+++ Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -6,11 +6,22 @@ public class AverageOfAList {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
 
-        // implement here a program, that first reads user input
-        // adding them on a list until user gives -1.
-        // Then it computes the average of the numbers on the list
-        // and prints it.
-        
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+            numbers.add(input);
+        }
+     
+        int sum = 0;
+        for (Integer number:numbers) {
+            sum += number;
+        }
+
+        System.out.println("Average: " + ((double) sum/numbers.size()));
+
     }
 }
