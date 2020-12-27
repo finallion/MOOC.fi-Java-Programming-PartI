b44b806b82dbefc12c662533cc51a81285de8bce
finallion <74151350+finallion@users.noreply.github.com>
2020-12-27 16:47:25 +0100

    Update SumOfArray.java

--- Part03_20.SumOfArray/src/main/java/SumOfArray.java
+++ Part03_20.SumOfArray/src/main/java/SumOfArray.java
@@ -2,13 +2,17 @@
 public class SumOfArray {
 
     public static void main(String[] args) {
-        // You can try the method here
         int[] array = {5, 1, 3, 4, 2};
         System.out.println(sumOfNumbersInArray(array));
     }
 
     public static int sumOfNumbersInArray(int[] array) {
-        // Write some code here
-        return 0;
+        int counter = 0;
+        int sum = 0;
+        while (counter < array.length) {
+            sum += array[counter];
+            counter++;
+        }
+        return sum;
     }
 }
