512613554e23f4fb49768cd0262fe4644337564d
finallion <74151350+finallion@users.noreply.github.com>
2020-12-27 16:48:09 +0100

    Update ArrayPrinter.java

--- Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
+++ Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
@@ -2,12 +2,21 @@
 public class ArrayPrinter {
 
     public static void main(String[] args) {
-        // You can test your method here
         int[] array = {5, 1, 3, 4, 2};
         printNeatly(array);
     }
 
     public static void printNeatly(int[] array) {
-        // Write some code in here
+        int counter = 0;
+        while (counter < array.length) {
+            if (counter == array.length - 1) {
+                System.out.println(array[counter]);
+                counter++;
+            } else {
+                System.out.print(array[counter] + ", ");
+                counter++;
+            }
+
+        }
     }
 }
