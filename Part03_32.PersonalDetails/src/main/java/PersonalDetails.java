6852a2bafab7cc803e83d7500027bef5fec84679
finallion <74151350+finallion@users.noreply.github.com>
2020-12-27 17:04:36 +0100

    Update PersonalDetails.java

--- Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,29 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int counter = 0;
+        int sum = 0;
+        int nameLength = 0;
+        String name = "";
 
+        while (true) {
+            String input = scanner.nextLine();
+            String[] pieces = input.split(",");
 
+            if (input.equals("")) {
+                System.out.println("Longest name: " + name);
+                System.out.println("Average of the birth years: " + (double) sum / counter);
+                break;
+            }
+
+            sum += Integer.valueOf(pieces[1]);
+            counter++;
+
+            if (pieces[0].length() > nameLength) {
+                nameLength = pieces[0].length();
+                name = pieces[0];
+            }
+        }
     }
 }
