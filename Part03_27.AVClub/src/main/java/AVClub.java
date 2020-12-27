15e226c513065759d91f4d32a181be794de29bbd
finallion <74151350+finallion@users.noreply.github.com>
2020-12-27 16:54:10 +0100

    Update AVClub.java

--- Part03_27.AVClub/src/main/java/AVClub.java
+++ Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,7 +5,19 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            String[] pieces = input.split(" ");
 
+            for (int x = 0; x < pieces.length; x++) {
+                if (pieces[x].contains("av")) {
+                    System.out.println(pieces[x]);
+                }
+            }
 
+            if (input.equals("")) {
+                break;
+            }
+        }
     }
 }
