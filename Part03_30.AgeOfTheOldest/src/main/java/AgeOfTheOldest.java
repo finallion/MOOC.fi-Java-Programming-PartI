7e6b1b751d9fe903ede3b2621e008164d9e2741e
finallion <74151350+finallion@users.noreply.github.com>
2020-12-27 16:58:12 +0100

    Update AgeOfTheOldest.java

--- Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -6,6 +6,21 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int x = 0;
+        while (true) {
+            String input = scanner.nextLine();
+            String[] parts = input.split(",");
 
+            if (input.equals("")) {
+                System.out.println("Age of the oldest: " + x);
+                break;
+            }
+
+            
+            int helper = Integer.valueOf(parts[1]);
+            if (helper > x) {
+                x = helper;
+            }
+        }     
     }
 }
