258e383b4df3a68ddf5fbb4b644dbceb9ca319ab
finallion <74151350+finallion@users.noreply.github.com>
2020-12-29 00:49:27 +0100

    Update Statistics.java

--- Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,26 @@
 
 public class Statistics {
+  
+    private int count;
+    private int sum;
+
+    public void addNumber(int number) {
+        count++; 
+        sum += number;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int sum() {
+        return this.sum;
+    }
+
+    public double average() {
+        if (count != 0) {
+            return (double) this.sum/this.count;
+        }
+        return 0;
+    }
 }
