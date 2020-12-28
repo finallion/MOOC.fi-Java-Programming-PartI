e127f94eb1802984b49a14dccf09976069c53609
finallion <74151350+finallion@users.noreply.github.com>
2020-12-29 00:54:03 +0100

    Update Items.java

--- Part04_17.Items/src/main/java/Items.java
+++ Part04_17.Items/src/main/java/Items.java
@@ -5,10 +5,18 @@ import java.util.Scanner;
 public class Items {
 
     public static void main(String[] args) {
-        // implement here your program that uses the class Item
-
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        
+        while (true) {
+            System.out.print("Name: ");
+            String input = scanner.nextLine();
+            
+            if (input.isEmpty()) {
+                break;
+            }
+            
+            items.add(new Item(input));
+        }
     }
 }
