83ac4c25fa371e788919d3e4fc5e7944acd93d9a
finallion <74151350+finallion@users.noreply.github.com>
2020-12-29 00:39:52 +0100

    Update DecreasingCounter.java

--- Part04_08.DecreasingCounter/src/main/java/DecreasingCounter.java
+++ Part04_08.DecreasingCounter/src/main/java/DecreasingCounter.java
@@ -1,21 +1,26 @@
 
 public class DecreasingCounter {
 
-    private int value;  // an object variable for storing the value of the counter
+    private int value;  
 
     public DecreasingCounter(int initialValue) {
         this.value = initialValue;
     }
 
     public void printValue() {
-        // Do not change this code!
         System.out.println("value: " + this.value);
     }
 
     public void decrement() {
-        // write the method implementation here
-        // the aim is to decrement the value of the counter by one
+        if (this.value > 0) { 
+            this.value--;
+        }
+    }
+    
+    
+    public void reset() {
+        this.value = 0;
     }
 
-    // the other methods go here
+
 }
