985bc6943c67b40814bf5c0556364ec0da768e28
finallion <74151350+finallion@users.noreply.github.com>
2020-12-29 00:30:29 +0100

    Update DogAttributes.java

--- Part04_03.DogAttributes/src/main/java/DogAttributes.java
+++ Part04_03.DogAttributes/src/main/java/DogAttributes.java
@@ -5,8 +5,6 @@ public class DogAttributes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Follow the instructions carefully
-        // and run the tests.
+        Dog dog = new Dog();
     }
 }
