a35d4a67791bf013773d17f0e5dd4b92eef00d8b
finallion <74151350+finallion@users.noreply.github.com>
2020-12-29 00:28:29 +0100

    Update YourFirstAccount.java

--- Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -2,7 +2,10 @@
 public class YourFirstAccount {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // Write your program here
+        
+        Account finallionsAccount = new Account ("Finallions Account", 100.0);
+        finallionsAccount.deposit(20);
+        System.out.println(finallionsAccount);
+        
     }
 }
