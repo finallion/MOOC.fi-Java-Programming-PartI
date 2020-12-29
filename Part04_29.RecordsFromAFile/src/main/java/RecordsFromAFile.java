c1756e7ed71b720b85303d76056efc0d6a73fd72
finallion <74151350+finallion@users.noreply.github.com>
2020-12-29 01:08:12 +0100

    Update RecordsFromAFile.java

--- Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,28 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+        
+        ArrayList<String> list = new ArrayList<>();
+
+        try ( Scanner reader = new Scanner(Paths.get(file))) {
+            while (reader.hasNextLine()) {
+                String read = reader.nextLine();
+                list.add(read);
+                String[] pieces = read.split(",");
+                
+                if (Integer.valueOf(pieces[1]) == 1) {
+                    System.out.println(pieces[0] + ", age: " + pieces[1] + " year");
+                } else {
+                    System.out.println(pieces[0] + ", age: " + pieces[1] + " years");
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error!");
+        }
 
     }
 }
