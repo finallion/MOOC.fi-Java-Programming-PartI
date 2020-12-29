4e9f37f85adfdb2b026742508278a7c3e6df9883
finallion <74151350+finallion@users.noreply.github.com>
2020-12-29 01:03:50 +0100

    Update PrintingASpecifiedFile.java

--- Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,17 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Which file should have its contents printed?");
+        String name = scanner.nextLine();
 
+        try(Scanner reader = new Scanner(Paths.get(name))) {
+            while (reader.hasNextLine()) {
+                String text = reader.nextLine();
+                System.out.println(text);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
