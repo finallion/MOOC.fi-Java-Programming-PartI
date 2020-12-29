b03ce3c432a5061656e8d83f2a4f8ff0878ffd54
finallion <74151350+finallion@users.noreply.github.com>
2020-12-29 01:00:12 +0100

    Update NumberOfStrings.java

--- Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,17 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int counter = 0;
 
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("end")) {
+                System.out.println(counter);
+                break;
+            }
+
+            counter++;
+        }
     }
 }
