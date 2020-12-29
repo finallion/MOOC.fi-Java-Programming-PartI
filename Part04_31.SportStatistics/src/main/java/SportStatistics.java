2d9f0e0d7b88e1fcdd57978aa47e2990fbfcae76
finallion <74151350+finallion@users.noreply.github.com>
2020-12-29 01:10:51 +0100

    Update SportStatistics.java

--- Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -7,6 +7,55 @@ public class SportStatistics {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        ArrayList<String> teams = new ArrayList<>();
+
+        System.out.println("File:");
+        String file = scan.nextLine();
+
+        System.out.println("Team:");
+        String team = scan.nextLine();
+        int counter = 0;
+        int win = 0;
+        int lose = 0;
+
+        try ( Scanner reader = new Scanner(Paths.get(file))) {
+            while (reader.hasNextLine()) {
+                String read = reader.nextLine();
+                teams.add(read);
+
+                String[] pieces = read.split(",");
+                String homeTeam = pieces[0];
+                String visitTeam = pieces[1];
+
+                int homePoints = Integer.valueOf(pieces[2]);
+                int visitPoints = Integer.valueOf(pieces[3]);
+
+
+                if (team.equals(homeTeam)) {
+                    counter++;
+                }
+
+                if (team.equals(visitTeam)) {
+                    counter++;
+                }
+
+                if ((team.equals(homeTeam)) && (homePoints > visitPoints)) {
+                    win++;
+                }
+
+                if ((team.equals(visitTeam)) && (visitPoints > homePoints)) {
+                    win++;
+                }
+            }
+
+            System.out.println("Games: " + counter);
+            System.out.println("Wins: " + win);
+            System.out.println("Losses: " + (counter-win));
+
+        } catch (Exception e) {
+             System.out.println("Error!");
+        }
 
     }
 
