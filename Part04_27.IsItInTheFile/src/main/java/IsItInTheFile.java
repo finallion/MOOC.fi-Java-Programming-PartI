b8f0492b9961cfb1e6089cd44e1658d883d11459
finallion <74151350+finallion@users.noreply.github.com>
2020-12-29 01:05:46 +0100

    Update IsItInTheFile.java

--- Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -12,6 +12,23 @@ public class IsItInTheFile {
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
+        
+        ArrayList<String> list = new ArrayList<>();
+        boolean printOnce = true;
+
+        try ( Scanner reader = new Scanner(Paths.get(file))) {
+            while (reader.hasNextLine()) {
+                list.add(reader.nextLine());
+            }
+            if (list.contains(searchedFor)) {
+                System.out.println("Found!");
+            } else if ((printOnce == true) && (!(list.contains(searchedFor)))) {
+                printOnce = false;
+                System.out.println("Not found.");
+            }
+        } catch (Exception e) {
+            System.out.println("Reading the file " + e.getMessage() + " failed.");
+        }
 
     }
 }
