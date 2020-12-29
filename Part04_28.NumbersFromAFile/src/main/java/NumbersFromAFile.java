8643b47d0af155ddc9e97da02b90326ef1f0ae5a
finallion <74151350+finallion@users.noreply.github.com>
2020-12-29 01:07:02 +0100

    Update NumbersFromAFile.java

--- Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -13,6 +13,25 @@ public class NumbersFromAFile {
         int lowerBound = Integer.valueOf(scanner.nextLine());
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
+        
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int counter = 0; 
+
+        try (Scanner reader = new Scanner(Paths.get(file))) {
+            while (reader.hasNextLine()) {
+                numbers.add(Integer.valueOf(reader.nextLine()));
+            }
+
+            for (Integer integer: numbers) {
+                if (integer >= lowerBound && integer <= upperBound) {
+                    counter++;
+                }
+            }
+            System.out.println("Numbers: " + counter);
+            
+        } catch (Exception e) {
+            System.out.println("Error!");
+        }
 
     }
 
