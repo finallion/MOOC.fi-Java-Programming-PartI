80c982c02e54633a0099c8310098e199c2104cdc
finallion <74151350+finallion@users.noreply.github.com>
2021-01-09 18:51:26 +0100

    Update LiquidContainers2.java

--- Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -6,16 +6,45 @@ public class LiquidContainers2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        Container first = new Container();
+        Container second = new Container();
 
         while (true) {
+            System.out.println("First: " + first);
+            System.out.println("Second: " + second);
             System.out.print("> ");
 
             String input = scan.nextLine();
+            
             if (input.equals("quit")) {
                 break;
             }
 
+            String[] pieces = input.split(" ");
+            String command = pieces[0];
+            
+            int amount = Integer.valueOf(pieces[1]);
+            
+            if (command.equals("add")) {
+                first.add(amount);
+            }
+
+            if (command.equals("move")) {
+                if (first.contains() - amount < 0){
+                    second.add(first.contains());
+                    first.remove(amount);
+                }
+
+                if (first.contains() - amount >= 0){
+                    first.remove(amount);
+                    second.add(amount);
+
+                }
+            }
+
+            if (command.equals("remove")) {
+                second.remove(amount);
+            }
         }
     }
-
 }
