86b8d7ac6ee54ec9fccd4dc680938addf62b6768
finallion <74151350+finallion@users.noreply.github.com>
2021-01-09 18:14:08 +0100

    Update Main.java

--- Part05_16.DatingApp/src/main/java/Main.java
+++ Part05_16.DatingApp/src/main/java/Main.java
@@ -2,6 +2,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        // test your program here!
+        SimpleDate date = new SimpleDate(0, 0, 0);
+
+        date.advance();
+        date.advance();
+        System.out.println(date);
+
+        date.advance(7);
+        System.out.println(date);
+
+        SimpleDate clone = date.afterNumberOfDays(9);
+        System.out.println(clone);
     }
 }
