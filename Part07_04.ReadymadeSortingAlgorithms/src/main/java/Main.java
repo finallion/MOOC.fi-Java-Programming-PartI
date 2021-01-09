8844c5fc276ea6b274177b9d0b47b642e29e968c
finallion <74151350+finallion@users.noreply.github.com>
2021-01-09 18:54:44 +0100

    Update Main.java

--- Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -6,5 +6,21 @@ public class Main {
         // insert test code here
         int[] array = {3, 1, 5, 99, 3, 12};
     }
+    
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sort(String[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sortIntegers(ArrayList<Integer> integers) {
+        Collections.sort(integers);
+    }
+
+    public static void sortStrings(ArrayList<String> strings) {
+        Collections.sort(strings);
+    }
 
 }
