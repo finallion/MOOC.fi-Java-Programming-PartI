ed7df7eb713b8569762109a8671f94b368fe907a
finallion <74151350+finallion@users.noreply.github.com>
2021-01-09 18:57:12 +0100

    Update Main.java

--- Part07_06.GradeStatistics/src/main/java/Main.java
+++ Part07_06.GradeStatistics/src/main/java/Main.java
@@ -5,8 +5,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Write your program here -- consider breaking the program into 
-        // multiple classes.
+
+        GradeStatistics statistics = new GradeStatistics();
+
+        UserInterface ui = new UserInterface(scanner, statistics);
+
+        ui.start();
     }
 }
