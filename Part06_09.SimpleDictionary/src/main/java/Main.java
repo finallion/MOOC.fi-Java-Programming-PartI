ea5a19e13c6d23dc51d45e5a097ecd710d48c851
finallion <74151350+finallion@users.noreply.github.com>
2021-01-09 18:36:18 +0100

    Update Main.java

--- Part06_09.SimpleDictionary/src/main/java/Main.java
+++ Part06_09.SimpleDictionary/src/main/java/Main.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Try out how the different parts of the program work together
-        // here
+        Scanner scanner = new Scanner(System.in);
+
+        SimpleDictionary dict = new SimpleDictionary();
+        TextUI ui = new TextUI(scanner, dict);
+        ui.start();
+
+        System.out.println(dict.translate("pike"));
     }
 }
