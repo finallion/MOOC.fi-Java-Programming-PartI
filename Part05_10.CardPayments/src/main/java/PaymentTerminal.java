53f8b67914822b0338bbda52011b00fd527a0939
finallion <74151350+finallion@users.noreply.github.com>
2021-01-09 18:05:16 +0100

    Update PaymentTerminal.java

--- Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -8,21 +8,58 @@ public class PaymentTerminal {
 
 
     public PaymentTerminal() {
-        // register initially has 1000 euros of money
+        this.money = 1000;
     }
 
     public double eatAffordably(double payment) {
-        // an affordable meal costs 2.50 euros
-        // increase the amount of cash by the price of an affordable mean and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment >= 2.5) {
+            affordableMeals++;
+            this.money = this.money + 2.5;
+            return payment - 2.5;
+        } else {
+            return payment;
+        }
     }
 
     public double eatHeartily(double payment) {
-        // a hearty meal costs 4.30 euros
-        // increase the amount of cash by the price of a hearty mean and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment >= 4.3) {
+            heartyMeals++;
+            this.money = this.money + 4.3;
+            return payment - 4.3;
+        } else {
+            return payment;
+        }
+    }
+
+ 
+
+    public boolean eatAffordably(PaymentCard card) {
+        if (card.balance() >= 2.5) {
+            card.takeMoney(2.5);
+            affordableMeals++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+ 
+    public boolean eatHeartily(PaymentCard card) {
+        if (card.balance() >= 4.3) {
+            card.takeMoney(4.3);
+            heartyMeals++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+ 
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if (sum >= 0) {
+            card.addMoney(sum);
+            this.money = this.money + sum;
+        }
     }
 
 
