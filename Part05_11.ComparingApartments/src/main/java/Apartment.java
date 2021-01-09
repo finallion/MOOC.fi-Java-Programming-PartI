4c7df9917babb131f2bc0a052bad6db7ffb55eac
finallion <74151350+finallion@users.noreply.github.com>
2021-01-09 18:06:19 +0100

    Update Apartment.java

--- Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -10,5 +10,26 @@ public class Apartment {
         this.squares = squares;
         this.princePerSquare = pricePerSquare;
     }
+    
+    public boolean largerThan(Apartment compared){
+        if (this.squares > compared.squares) {
+            return true;
+        }
+        return false;
+    }
 
+    public int priceDifference(Apartment compared) {
+        if ((this.squares * this.princePerSquare) > (compared.squares * compared.princePerSquare)) {
+            return (this.squares * this.princePerSquare) - (compared.squares * compared.princePerSquare);
+        } else {
+            return(compared.squares * compared.princePerSquare) - (this.squares * this.princePerSquare);
+        }
+    }  
+
+    public boolean moreExpensiveThan(Apartment compared) {
+        if ((this.squares * this.princePerSquare) > (compared.squares * compared.princePerSquare)) {
+            return true;
+        }
+        return false;
+    }
 }
