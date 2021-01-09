de4349314ec2044ada8bc86c4c73aae159168896
finallion <74151350+finallion@users.noreply.github.com>
2021-01-09 17:53:21 +0100

    Update Program.java

--- Part05_03.Cube/src/main/java/Program.java
+++ Part05_03.Cube/src/main/java/Program.java
@@ -6,6 +6,9 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Experiment with your program here
+        Cube cubeOne = new Cube(6);
+
+        System.out.println(cubeOne.volume());
+        System.out.println(cubeOne);
     }
 }
