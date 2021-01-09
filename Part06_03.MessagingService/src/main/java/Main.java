311d6d111bd759b323b863333b8e80be615bc961
finallion <74151350+finallion@users.noreply.github.com>
2021-01-09 18:21:02 +0100

    Update Main.java

--- Part06_03.MessagingService/src/main/java/Main.java
+++ Part06_03.MessagingService/src/main/java/Main.java
@@ -2,7 +2,10 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        // Try out your class here
+        MessagingService ms = new MessagingService();
+        
+        System.out.println(ms.getMessages());
+        ms.add(new Message("Github", "Hi"));
+        System.out.println(ms.getMessages());
     }
 }
